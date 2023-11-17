9778756015c2b4502feb7f1af3a5e2778af6f61f
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-11-17 22:03:21 +0100

    Update Fuse.py

--- Fuse.py
+++ Fuse.py
@@ -1,22 +1,20 @@
 import string
+import loadinganim
 alphabet = list(string.ascii_uppercase)
-alphabet.append(' ')
 word = list(str(input().upper()))
 word_num = []
 
 for letter in word:
-	word_num.append(alphabet.index(letter)+1)
+	word_num.append(ord(letter))
+
+loadinganim.clear()
+loadinganim.start(5)
+done = False
 
 for n in word_num:
-	try:
-		if n == 27:
-			print(' ',end='')
-		else:
-			n = str(n)
-			sn = int(len(n)/2)
-			res = int(n[:sn])+int(n[sn:])
-			print(alphabet[res],end='')
-	except:
-		print(alphabet[int(n)],end='')
+	n = str(n)
+	sn = int(len(n)/2)
+	res = int(n[:sn])+int(n[sn:])
+	print(alphabet[res],end='')
 		
 #It is an cyphering system that assigns each letter of the given word its corresponding ASCII value, divides this value in half, and then combines the results to obtain a new value, which is then converted back into a letter of the alphabet.
